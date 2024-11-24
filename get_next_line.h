6eb437734c80cf5901fde4996fb3bfe630871d12
Moac <benjamsc@student.42.fr>
2024-11-24 21:04:21 +0100

    new

---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: benjamsc <benjamsc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 11:17:52 by benjamsc          #+#    #+#             */
+/*   Updated: 2024/11/24 09:39:50 by benjamsc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 4
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdint.h>
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstlast(t_list *lst);
+int		found_nwline(t_list *storage);
+void	alloc_size_line(char **ptr_line, t_list *storage);
+void	free_all_storage(t_list *stash);
+int		ft_strlen(char *str);
+
+#endif // !GET_NEXT_LINE_H
